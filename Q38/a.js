0f7511314467538761cea450f18d9ff0a8db83d6
KIMHYUNGJU89 <152847964+KIMHYUNGJU89@users.noreply.github.com>
2023-12-07 14:52:11 +0900

    로또2(당첨번호까지)
    js에서 미리 내가 뽑을수를 설정해서 당첨갯수까지 표시하게했다.

---
+++ Q38/a.js
@@ -0,0 +1,150 @@
+var a = 20; //변수
+var b = 24;
+var c = 11;
+var d = 8;
+var e = 17;
+var f = 26;
+
+
+    var x1 = Math.floor(Math.random()*45)+1; //1~45까지에서 랜덤으로 뽑아주는 명령어
+    document.write(x1);
+    document.write("<br>");
+    var x2 = Math.floor(Math.random()*45)+1;
+    document.write(x2);
+    document.write("<br>");
+    var x3 = Math.floor(Math.random()*45)+1;
+    document.write(x3);
+    document.write("<br>");
+    var x4 = Math.floor(Math.random()*45)+1;
+    document.write(x4);
+    document.write("<br>");
+    var x5 = Math.floor(Math.random()*45)+1;
+    document.write(x5);
+    document.write("<br>");
+    var x6 = Math.floor(Math.random()*45)+1;
+    document.write(x6);
+    document.write("<br>");
+
+var win = 0; //내가맞춘수
+
+    //1
+    if( a == x1) {
+        win=win+1;
+    }
+    if( a == x2) {
+        win=win+1;
+    }
+    if( a == x3) {
+        win=win+1;
+    }
+    if( a == x4) {
+        win=win+1;
+    }
+    if( a == x5) {
+        win=win+1;
+    }
+    if( a == x6) {
+        win=win+1;
+    }
+    
+    //2
+    if( b == x1) {
+        win=win+1;
+    }
+    if( b == x2) {
+        win=win+1;
+    }
+    if( b == x3) {
+        win=win+1;
+    }
+    if( b == x4) {
+        win=win+1;
+    }
+    if( b == x5) {
+        win=win+1;
+    }
+    if( b == x6) {
+        win=win+1;
+    }
+    
+    //3
+    if( c == x1) {
+        win=win+1;
+    }
+    if( c == x2) {
+        win=win+1;
+    }
+    if( c == x3) {
+        win=win+1;
+    }
+    if( c == x4) {
+        win=win+1;
+    }
+    if( c == x5) {
+        win=win+1;
+    }
+    if( c == x6) {
+        win=win+1;
+    }
+    
+    //4
+    if( d == x1) {
+        win=win+1;
+    }
+    if( d == x2) {
+        win=win+1;
+    }
+    if( d == x3) {
+        win=win+1;
+    }
+    if( d == x4) {
+        win=win+1;
+    }
+    if( d == x5) {
+        win=win+1;
+    }
+    if( d == x6) {
+        win=win+1;
+    }
+    
+    //5
+    if( e == x1) {
+        win=win+1;
+    }
+    if( e == x2) {
+        win=win+1;
+    }
+    if( e == x3) {
+        win=win+1;
+    }
+    if( e == x4) {
+        win=win+1;
+    }
+    if( e == x5) {
+        win=win+1;
+    }
+    if( e == x6) {
+        win=win+1;
+    }
+    
+    //6
+    if( f == x1) {
+        win=win+1;
+    }
+    if( f == x2) {
+        win=win+1;
+    }
+    if( f == x3) {
+        win=win+1;
+    }
+    if( f == x4) {
+        win=win+1;
+    }
+    if( f == x5) {
+        win=win+1;
+    }
+    if( f == x6) {
+        win=win+1;
+    }
+    
+    document.write("당첨갯수:"+win);
