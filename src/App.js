4d9a8a7e9521c6fc883edc9b3e1458c0174799bc
DESKTOP-U7IVOBB\user <gimhyeongju89@gmail.com>
2024-06-10 11:28:43 +0900

    2024-06-09일자 react 연습

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+  let post = '강남 우동 맛집';
+  let [a,b] = useState('남자 코트 추천')
   return (
     <div className="App">
       <div className="black-nav"> 
-        <h4>블로그임</h4>
+        <h4 style={ {color : 'red', fontSize : '16px'} }>블로그임</h4>
       </div>
-      <p>
-
-      </p>
+      <div className='list'>
+        <h4>{a}</h4>
+        <p>2월 17일 발행</p>
+      </div>
     </div>
   );
 }
