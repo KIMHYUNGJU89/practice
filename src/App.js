1eee633d86d1023f1241400f0547d034f483d31a
DESKTOP-U7IVOBB\user <gimhyeongju89@gmail.com>
2024-06-11 22:57:45 +0900

    react props,input  기능숙지

--- src/App.js
+++ src/App.js
@@ -3,21 +3,21 @@ import './App.css';
 import { useState } from 'react';
 
 function App() {
-  
+  let dates = nowDate();
   let[list,setList] = useState(['페이트 제로','페이트 스테이 나이트','페이트 엑스텔라']);
   let listCopy = [...list];
   let[good,setGood] = useState([]);
+  let[date,setDate] = useState([]);
   let[title,setTitle] = useState(0);
   let[modal,setModal] = useState(false);
   let modalCheck = true ;
-
-
+  let [userInput,inputSet] = useState('');
   return (
     <div className="App">
     <button onClick={() => {
       listCopy[2] = '페이트 그랜드 오더';
       setList(listCopy);
-    }}>글제목수정</button>
+      }}>글제목수정</button>
     <button onClick={() => {
       listCopy.sort();
       setList(listCopy);
@@ -27,6 +27,7 @@ function App() {
     function(titleName,i)
     {
     good.push(0);
+    date.push(dates);
     return(
     <div className='list'>
     <h4 onClick={
@@ -39,19 +40,33 @@ function App() {
       }
     }>
     {titleName}
-    <span onClick={() => {
+    <span onClick={(e) => {
+      e.stopPropagation();
       let goodCopy = [...good];
       goodCopy[i] = goodCopy[i] + 1;
       setGood(goodCopy)
-      }}>👍</span>{good[i]}
+      }}>👍{good[i]}</span>
     </h4>
-
-    <p>2024-06-11 발행</p>
+    <p>{date[i]}</p>
+    <button onClick={() => {
+      listCopy.splice(i,1);
+      setList(listCopy);
+    }}>글 삭제</button>
     </div>
     )
     })    
     }
-    <input></input>
+    <input onChange={(e) => 
+      {inputSet(e.target.value); 
+    }
+    }/>
+    <button onClick={()=>{
+      if(userInput !== ''){
+        listCopy.unshift(userInput);
+        }
+      setList(listCopy);
+        
+    }}>글입력</button>
     {
       modal == true ? <Modal title = {title} list = {list} setList = {
         () => {
@@ -59,7 +74,7 @@ function App() {
         setList(listCopy);
        }
     } 
-    color={'yellow'} ></Modal> : null
+    color={'yellow'} /> : null 
     }
     </div>
 
@@ -75,6 +90,12 @@ function Modal(props) {
       <button onClick={props.setList}>글수정</button>
     </div>
   );
+}
+
+function nowDate (){
+  let dates = new Date();
+  let months = dates.getMonth()+1;
+  return dates.getFullYear() + " 년 " + months+ " 월 " + dates.getDate() + " 일 ";
 }
 
 
